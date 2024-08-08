bc974d91cc8ed899b32f112c7ba14108e2cae4b2
Noboru <163944578+EuAmoMilho@users.noreply.github.com>
2024-08-08 11:57:00 -0300

    Update Disciplina.java

--- src/app/Disciplina.java
+++ src/app/Disciplina.java
@@ -1,5 +1,23 @@
 package app;
 
 public class Disciplina {
-
+  private String nome,
+				   codigo;
+	
+	public Disciplina(String nome, String codigo) {
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public String toString() {
+		String resposta = "DISCIPLINA: " + nome + "\n" +"CODIGO DA DISCIPLINA: " + codigo + "\n";
+		return resposta;
+	}
 }
