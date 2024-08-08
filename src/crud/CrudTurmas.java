c1b2793638deab7da21c2b79cb86fdbdab4d4bd4
Noboru <163944578+EuAmoMilho@users.noreply.github.com>
2024-08-08 11:59:33 -0300

    (contem erros)CrudTurmas.java
    quando chama a funcao "pesquisar disciplina" ele retorna nullpointer exception

--- src/crud/CrudTurmas.java
+++ src/crud/CrudTurmas.java
@@ -1,5 +1,53 @@
-package cadastros;
+package crud;
 
-public class CrudTurmas {
+import java.util.List;
+import java.util.LinkedList;
 
+import app.Disciplina;
+
+public class CrudDisciplina {
+	int numDisc;
+	private List<Disciplina> disciplinas;
+	
+	public CrudDisciplina(){
+		numDisc = 0;
+		disciplinas = new LinkedList<Disciplina>();
+	}
+	
+	public int cadastrarDisciplina(Disciplina d) {
+		boolean cadastrou = disciplinas.add(d);
+		if(cadastrou) {
+			numDisc = disciplinas.size();
+		}
+			return numDisc;
+	}
+	
+	public Disciplina pesquisarDisciplina(String codigo) {
+		Disciplina pesquisar = null;
+		 for(Disciplina d : disciplinas) {
+			 if (d.getCodigo().equalsIgnoreCase(codigo)) {
+				 return pesquisar;
+			 }
+		 }
+		 return pesquisar;
+	}
+	
+	public boolean removerDisciplina(Disciplina d) {
+		boolean removeu = false;
+		if(disciplinas.contains(d)) {
+			removeu = disciplinas.remove(d);
+			numDisc = disciplinas.size();
+		}
+		return removeu;
+	}
+	
+	public boolean atualizarDisciplina(String codigo, Disciplina d) {
+		for(Disciplina atualizar : disciplinas) {
+			if(atualizar.getCodigo().equalsIgnoreCase(codigo)) {
+				atualizar = d;
+				return true;
+			}
+		}
+		return false;
+	}
 }
