f7e75a2754160fe3b639164c6522822a7956b37b
Noboru <163944578+EuAmoMilho@users.noreply.github.com>
2024-08-08 11:54:17 -0300

    Update Turma.java

--- src/app/Turma.java
+++ src/app/Turma.java
@@ -1,5 +1,67 @@
 package app;
 
-public class Turma {
+import java.util.List;
 
+public class Turma {
+  
+private String codigo;
+	private int qtdVagas,
+				qtdVagasLivres;
+	
+	private Professor professorAssociado;
+	private List<Aluno> alunosMatriculados;
+	private Disciplina disciplina;
+	
+	public Turma(String codigo, int qtdVagas) {
+		
+	}
+	
+	public final String getCodigo() {
+		return codigo;
+	}
+	
+	public final int getQtdVagas() {
+		return qtdVagas;
+	}
+	
+	public final int getQtdVagasLivres() {
+		return qtdVagasLivres;
+	}
+	
+	public final Professor getProfessor() {
+		return professorAssociado;
+	}
+	
+	public final List<Aluno> getAlunosMatriculados(){
+		return alunosMatriculados;
+	}
+	
+	public final Disciplina getDisciplina() {
+		return disciplina;
+	}
+	
+	public final void setQtdVagasLivres(int qtdVagasLivres) {
+		this.qtdVagasLivres = qtdVagasLivres;
+	}
+	
+	public void setProfessor(Professor professorAssociado) {
+		this.professorAssociado = professorAssociado;
+	}
+	
+	public void setAlunos(List<Aluno> alunosMatriculados) {
+		this.alunosMatriculados = alunosMatriculados;
+	}
+	
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	public final String toString() {
+		String resposta = "DISCIPLINA: " + disciplina.getCodigo() + " " +disciplina.getNome()+ "\n";
+		resposta += "CODIGO: " + codigo + "\n";
+		resposta += "QUANTIDADE DE VAGAS: " + qtdVagas + "\n";
+		resposta += "NUMERO DE VAGAS LIVRES: " + qtdVagasLivres + "\n";
+		resposta += "PROFESSOR: " + professorAssociado.getNome() + "\n";
+		return resposta;
+	}
 }
