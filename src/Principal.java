2fde1a92132a50c7527ad595be932c3fa0973be0
Noboru <163944578+EuAmoMilho@users.noreply.github.com>
2024-08-11 23:20:27 -0300

    Create Principal.java

---
+++ src/Principal.java
@@ -0,0 +1,75 @@
+import javax.swing.JOptionPane;
+import crud.CrudAlunos;
+import crud.CrudProfessores;
+import crud.CrudDisciplina;
+import crud.CrudTurma;
+import excecao.CampoEmBrancoException;
+import excecao.DisciplinaNaoAtribuidaException;
+import excecao.ProfessorNaoAtribuidoException;
+import view.MenuAluno;
+import view.MenuDisciplina;
+import view.MenuProfessor;
+import view.MenuTurma;
+import view.MenuPrincipal;
+
+public class Principal {
+
+	static CrudAlunos crudAluno;
+	static CrudProfessores crudProfessor;
+	static CrudTurma crudTurma;
+	static CrudDisciplina crudDisciplina;
+
+	public static void main(String[] args) {
+		crudAluno = new CrudAlunos();
+		crudProfessor = new CrudProfessores();
+		crudDisciplina = new CrudDisciplina();
+		crudTurma = new CrudTurma(crudAluno, crudProfessor, crudDisciplina);
+
+		int opcao = 0;
+
+		do {
+			opcao = MenuPrincipal.menuOpcoes();
+			switch (opcao) {
+				case 1:
+				try {
+					MenuAluno.menuAluno(crudAluno);
+				} catch (CampoEmBrancoException e) {
+					e.printStackTrace();
+				}
+					break;
+				case 2:
+				try {
+					MenuProfessor.menuProfessor(crudProfessor);
+				} catch (CampoEmBrancoException e) {
+					e.printStackTrace();
+				}
+					break;
+				case 3:
+				try {
+					MenuDisciplina.menuDisciplina(crudDisciplina, crudTurma);
+				} catch (CampoEmBrancoException e) {
+					e.printStackTrace();
+				}
+					break;
+				case 4:
+				try {
+					MenuTurma.menuTurma(crudTurma);
+				} catch (CampoEmBrancoException e) {
+					e.printStackTrace();
+				} catch (DisciplinaNaoAtribuidaException e) {
+					e.printStackTrace();
+				} catch (ProfessorNaoAtribuidoException e) {
+					e.printStackTrace();
+				}
+					break;
+				case 0:
+					break;
+				default:
+					JOptionPane.showMessageDialog(null, "Opcao invalida");
+					opcao = -1;
+					break;
+			}
+		} while (opcao != 0);
+		return;
+	}
+}
